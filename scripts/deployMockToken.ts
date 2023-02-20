99a517bf890d2aec77fd7f010d96ca48211b55da
greedythib <thibaud.bruyelle@gmail.com>
2023-02-20 09:52:40 +0100

    feat: hardhat script for creating/depositing rewards

---
+++ scripts/deployMockToken.ts
@@ -0,0 +1,35 @@
+import { parseEther } from 'ethers/lib/utils';
+import { ethers } from 'hardhat';
+
+import { MAX_UINT256 } from '../test/hardhat/utils/helpers';
+import { MockToken__factory } from '../typechain';
+
+async function main() {
+  const { deployer } = await ethers.getNamedSigners();
+  const distributorCreator = `0xA9c076992F3917b47E2C619c46ff0b652d76e6B4`;
+  console.log(`DistributorCreator address ${distributorCreator}`);
+
+  console.log(`Deploying MockToken with address ${deployer.address}...`);
+  const MockToken = await new MockToken__factory(deployer).deploy('MockAngleReward', 'agReward', 18);
+  // const MockToken = MockToken__factory.connect('0x84FB94595f9Aef81147cD4070a1564128A84bb7c', deployer);
+  console.log('...Deployed mock token at address', MockToken.address, '✅');
+
+  console.log(`Minting MockToken to ${deployer.address}...`);
+  await (
+    await MockToken.mint(deployer.address, parseEther('5000'), {
+      gasLimit: 300_000,
+      maxPriorityFeePerGas: 100e9,
+      maxFeePerGas: 700e9,
+    })
+  ).wait();
+  console.log('...Minted mock token to address', deployer.address, '✅');
+
+  console.log('Approving...');
+  await (await MockToken.connect(deployer).approve(distributorCreator, MAX_UINT256)).wait();
+  console.log(`...Approved reward manager ${distributorCreator} succesfully ✅`);
+}
+
+main().catch(error => {
+  console.error(error);
+  process.exit(1);
+});
--- scripts/sendReward.ts
+++ scripts/sendReward.ts
@@ -1,49 +1,43 @@
 import { parseEther } from 'ethers/lib/utils';
-import { deployments, ethers, web3 } from 'hardhat';
+import { ethers, web3 } from 'hardhat';
 
-import { MAX_UINT256, ZERO_ADDRESS } from '../test/hardhat/utils/helpers';
-import { DistributionCreator, DistributionCreator__factory, MockToken, MockToken__factory } from '../typechain';
+import { ZERO_ADDRESS } from '../test/hardhat/utils/helpers';
+import { DistributionCreator__factory, MockCoreBorrow__factory } from '../typechain';
 
 async function main() {
-  let manager: DistributionCreator;
-  let mockToken: MockToken;
   const { deployer } = await ethers.getNamedSigners();
 
-  const managerAddress = (await deployments.get('DistributionCreator')).address;
-  const mockTokenAddress = '0x02Cb0586F9252626e992B2C6c1B792d9751f2Ede';
-  const uniV3agEURUSDC = '0x3fa147d6309abeb5c1316f7d8a7d8bd023e0cd80';
+  const distributorCreator = `0xA9c076992F3917b47E2C619c46ff0b652d76e6B4`;
+  const mockTokenAddress = '0x84FB94595f9Aef81147cD4070a1564128A84bb7c';
+  const pool = '0x3fa147d6309abeb5c1316f7d8a7d8bd023e0cd80';
 
-  manager = new ethers.Contract(
-    managerAddress,
-    DistributionCreator__factory.createInterface(),
-    deployer,
-  ) as DistributionCreator;
-  mockToken = new ethers.Contract(mockTokenAddress, MockToken__factory.createInterface(), deployer) as MockToken;
+  const manager = DistributionCreator__factory.connect(distributorCreator, deployer);
+  const mockToken = MockCoreBorrow__factory.connect(mockTokenAddress, deployer);
 
   const params = {
-    uniV3Pool: uniV3agEURUSDC,
+    uniV3Pool: pool,
     rewardToken: mockToken.address,
-    positionWrappers: [],
-    wrapperTypes: [],
-    amount: parseEther('1'),
+    positionWrappers: ['0x1644de0A8E54626b54AC77463900FcFFD8B94542', '0xa29193Af0816D43cF44A3745755BF5f5e2f4F170'],
+    wrapperTypes: [0, 2],
+    amount: parseEther('350'),
     propToken0: 4000,
     propToken1: 2000,
     propFees: 4000,
     isOutOfRangeIncentivized: 0,
-    // 25th of January at 8pm -> change this
-    epochStart: 1675698166,
-    numEpoch: 1000,
+    epochStart: 1676649600,
+    numEpoch: 500,
     boostedReward: 0,
     boostingAddress: ZERO_ADDRESS,
-    rewardId: web3.utils.soliditySha3('testing') as string,
-    additionalData: web3.utils.soliditySha3('test2ng') as string,
+    rewardId: web3.utils.soliditySha3('europtimism') as string,
+    additionalData: web3.utils.soliditySha3('europtimism') as string,
   };
 
-  console.log('Approving');
-  await (await mockToken.connect(deployer).approve(manager.address, MAX_UINT256)).wait();
+  // console.log('Approving');
+  // await (await mockToken.connect(deployer).approve(manager.address, MAX_UINT256)).wait();
 
-  console.log('Depositing reward');
-  await (await manager.connect(deployer).createDistribution(params)).wait();
+  console.log('Depositing reward...');
+  await (await manager.connect(deployer).createDistribution(params, { gasLimit: 1e6 })).wait();
+  console.log('...Deposited reward ✅');
 }
 
 main().catch(error => {
