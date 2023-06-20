3cde30ee773fc8189efc4b1b788fbbcce97701b2
Pablo Veyrat <veyratpablo@gmail.com>
2023-06-20 18:49:00 +0200

    fix: Merkl CI

--- contracts/deprecated/OldDistributionCreator.sol
+++ contracts/deprecated/OldDistributionCreator.sol
@@ -19,6 +19,7 @@ import "../struct/RewardTokenAmounts.sol";
 /// @dev This contract is mostly a helper for APIs built on top of Merkl
 /// @dev People depositing rewards must have signed a `message` with the conditions for using the
 /// product
+//solhint-disable
 contract OldDistributionCreator is UUPSHelper, ReentrancyGuardUpgradeable {
     using SafeERC20 for IERC20;
 
