55bf376fed3d461d361124a883b222b484a861f4
picodes <picodes@angle.money>
2023-06-20 18:23:22 +0200

    feat: change dispute period

--- contracts/Distributor.sol
+++ contracts/Distributor.sol
@@ -62,6 +62,9 @@ struct Claim {
 contract Distributor is UUPSHelper {
     using SafeERC20 for IERC20;
 
+    /// @notice Epoch duration
+    uint32 public constant EPOCH_DURATION = 3600;
+
     // ================================= VARIABLES =================================
 
     /// @notice Tree of claimable tokens through this contract
@@ -80,10 +83,10 @@ contract Distributor is UUPSHelper {
     /// @dev Used to store if there is an ongoing dispute
     address public disputer;
 
-    /// @notice Last time the `tree` was updated
-    uint48 public lastTreeUpdate;
+    /// @notice When the current tree will become valid
+    uint48 public endOfDisputePeriod;
 
-    /// @notice Time before which a change in a tree becomes effective
+    /// @notice Time before which a change in a tree becomes effective, in EPOCH_DURATION
     uint48 public disputePeriod;
 
     /// @notice Amount to deposit to freeze the roots update
@@ -106,15 +109,16 @@ contract Distributor is UUPSHelper {
     // =================================== EVENTS ==================================
 
     event Claimed(address user, address token, uint256 amount);
+    event DisputeAmountUpdated(uint256 _disputeAmount);
     event Disputed(string reason);
     event DisputePeriodUpdated(uint48 _disputePeriod);
-    event DisputeTokenUpdated(address indexed _disputeToken);
-    event DisputeAmountUpdated(uint256 _disputeAmount);
     event DisputeResolved(bool valid);
+    event DisputeTokenUpdated(address indexed _disputeToken);
     event OperatorClaimingToggled(address user, bool isEnabled);
     event OperatorToggled(address user, address operator, bool isWhitelisted);
     event Recovered(address indexed token, address indexed to, uint256 amount);
-    event TreeUpdated(bytes32 merkleRoot, bytes32 ipfsHash);
+    event Revoked(); // With this event an indexer could maintain a table (timestamp, merkleRootUpdate)
+    event TreeUpdated(bytes32 merkleRoot, bytes32 ipfsHash, uint48 endOfDisputePeriod);
     event TrustedToggled(address indexed eoa, bool trust);
 
     // ================================= MODIFIERS =================================
@@ -193,7 +197,7 @@ contract Distributor is UUPSHelper {
 
     /// @notice Returns the MerkleRoot that is currently live for the contract
     function getMerkleRoot() public view returns (bytes32) {
-        if (block.timestamp - lastTreeUpdate >= disputePeriod) return tree.merkleRoot;
+        if (block.timestamp >= endOfDisputePeriod) return tree.merkleRoot;
         else return lastTree.merkleRoot;
     }
 
@@ -212,21 +216,23 @@ contract Distributor is UUPSHelper {
             disputer != address(0) ||
             // A trusted address cannot update a tree right after a precedent tree update otherwise it can de facto
             // validate a tree which has not passed the dispute period
-            ((canUpdateMerkleRoot[msg.sender] != 1 || block.timestamp - lastTreeUpdate < disputePeriod) &&
+            ((canUpdateMerkleRoot[msg.sender] != 1 || block.timestamp < endOfDisputePeriod) &&
                 !core.isGovernorOrGuardian(msg.sender))
         ) revert NotTrusted();
         MerkleTree memory _lastTree = tree;
         tree = _tree;
         lastTree = _lastTree;
-        lastTreeUpdate = uint48(block.timestamp);
-        emit TreeUpdated(_tree.merkleRoot, _tree.ipfsHash);
+
+        uint48 _endOfPeriod = _endOfDisputePeriod(uint48(block.timestamp));
+        endOfDisputePeriod = _endOfPeriod;
+        emit TreeUpdated(_tree.merkleRoot, _tree.ipfsHash, _endOfPeriod);
     }
 
     /// @notice Freezes the Merkle tree update until the dispute is resolved
     /// @dev Requires a deposit of `disputeToken` that'll be slashed if the dispute is not accepted
     /// @dev It is only possible to create a dispute for `disputePeriod` after each tree update
     function disputeTree(string memory reason) external {
-        if (block.timestamp - lastTreeUpdate >= disputePeriod) revert InvalidDispute();
+        if (block.timestamp >= endOfDisputePeriod) revert InvalidDispute();
         IERC20(disputeToken).safeTransferFrom(msg.sender, address(this), disputeAmount);
         disputer = msg.sender;
         emit Disputed(reason);
@@ -242,7 +248,7 @@ contract Distributor is UUPSHelper {
             _revokeTree();
         } else {
             IERC20(disputeToken).safeTransfer(msg.sender, disputeAmount);
-            lastTreeUpdate = uint48(block.timestamp);
+            endOfDisputePeriod = _endOfDisputePeriod(uint48(block.timestamp));
         }
         disputer = address(0);
         emit DisputeResolved(valid);
@@ -270,18 +276,13 @@ contract Distributor is UUPSHelper {
     }
 
     /// @notice Recovers any ERC20 token
-    function recoverERC20(
-        address tokenAddress,
-        address to,
-        uint256 amountToRecover
-    ) external onlyGovernorOrGuardian {
+    function recoverERC20(address tokenAddress, address to, uint256 amountToRecover) external onlyGovernorOrGuardian {
         IERC20(tokenAddress).safeTransfer(to, amountToRecover);
         emit Recovered(tokenAddress, to, amountToRecover);
     }
 
     /// @notice Sets the dispute period before which a tree update becomes effective
     function setDisputePeriod(uint48 _disputePeriod) external onlyGovernorOrGuardian {
-        if (_disputePeriod > block.timestamp) revert InvalidParam();
         disputePeriod = uint48(_disputePeriod);
         emit DisputePeriodUpdated(_disputePeriod);
     }
@@ -305,9 +306,20 @@ contract Distributor is UUPSHelper {
     /// @notice Fallback to the last version of the tree
     function _revokeTree() internal {
         MerkleTree memory _tree = lastTree;
-        lastTreeUpdate = 0;
+        endOfDisputePeriod = 0;
         tree = _tree;
-        emit TreeUpdated(_tree.merkleRoot, _tree.ipfsHash);
+        emit Revoked();
+        emit TreeUpdated(
+            _tree.merkleRoot,
+            _tree.ipfsHash,
+            (uint48(block.timestamp) / EPOCH_DURATION) * (EPOCH_DURATION) // Last hour
+        );
+    }
+
+    /// @notice Returns the end of the dispute period
+    /// @dev treeUpdate is rounded up to next hour and then `disputePeriod` hours are added
+    function _endOfDisputePeriod(uint48 treeUpdate) internal view returns (uint48) {
+        return ((treeUpdate - 1) / EPOCH_DURATION + 1 + disputePeriod) * (EPOCH_DURATION);
     }
 
     /// @notice Checks the validity of a proof
