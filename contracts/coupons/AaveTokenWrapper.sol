f3fb359d5d21773ce98b112d49f35ddc84a33450
Pablo Veyrat <veyratpablo@gmail.com>
2024-04-03 19:57:31 +0200

    v1 of Merkl contract

---
+++ contracts/coupons/AaveTokenWrapper.sol
@@ -0,0 +1,109 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity ^0.8.17;
+
+import { ERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+
+import "../utils/UUPSHelper.sol";
+
+contract AaveTokenWrapper is UUPSHelper, ERC20Upgradeable {
+    using SafeERC20 for IERC20;
+
+    // ================================= VARIABLES =================================
+
+    /// @notice `Core` contract handling access control
+    ICore public core;
+
+    // could be put as immutable in non upgradeable contract
+    address public token;
+    address public distributor;
+    address public feeManager;
+
+    mapping(address => uint256) public isMasterClaimer;
+    mapping(address => address) public delegateReceiver;
+
+    // =================================== EVENTS ==================================
+
+    event Recovered(address indexed token, address indexed to, uint256 amount);
+
+    // ================================= MODIFIERS =================================
+
+    /// @notice Checks whether the `msg.sender` has the governor role or the guardian role
+    modifier onlyGovernor() {
+        if (!core.isGovernor(msg.sender)) revert NotGovernor();
+        _;
+    }
+
+    // ================================= FUNCTIONS =================================
+
+    function initialize(
+        address underlyingToken,
+        address _distributor,
+        address _feeManager,
+        address _core
+    ) public initializer {
+        // TODO could fetch name and symbol based on real token
+        __ERC20_init("AaveTokenWrapper", "ATW");
+        __UUPSUpgradeable_init();
+        if (underlyingToken == address(0) || _distributor == address(0) || _feeManager == address(0))
+            revert ZeroAddress();
+        token = underlyingToken;
+        feeManager = _feeManager;
+        distributor = _distributor;
+        ICore(_core).isGovernor(msg.sender);
+        core = ICore(_core);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal override {
+        // Needs an approval before hand, this is how mints are done
+        if (to == distributor) {
+            IERC20(token).safeTransferFrom(from, address(this), amount);
+            _mint(from, amount); // These are then transfered to the distributor
+        }
+        if (to == feeManager) {
+            IERC20(token).safeTransferFrom(from, feeManager, amount);
+            _mint(from, amount); // These are then transferred to the fee manager
+        }
+    }
+
+    function _afterTokenTransfer(address from, address to, uint256 amount) internal override {
+        if (from == address(distributor)) {
+            if (tx.origin == to || isMasterClaimer[msg.sender] == 1) {
+                _handleClaim(to, amount);
+            } else if (allowance(to, tx.origin) > amount) {
+                _spendAllowance(to, tx.origin, amount);
+                _handleClaim(to, amount);
+            }
+        }
+    }
+
+    function _handleClaim(address to, uint256 amount) internal {
+        address delegate = delegateReceiver[to];
+        _burn(to, amount);
+        if (delegate == address(0) || delegate == to) {
+            IERC20(token).safeTransfer(to, amount);
+        } else {
+            IERC20(token).safeTransfer(delegate, amount);
+        }
+    }
+
+    /// @notice Recovers any ERC20 token
+    function recoverERC20(address tokenAddress, address to, uint256 amountToRecover) external onlyGovernor {
+        IERC20(tokenAddress).safeTransfer(to, amountToRecover);
+        emit Recovered(tokenAddress, to, amountToRecover);
+    }
+
+    function toggleMasterClaimer(address claimer) external onlyGovernor {
+        uint256 claimStatus = 1 - isMasterClaimer[claimer];
+        isMasterClaimer[claimer] = claimStatus;
+    }
+
+    function updateDelegateReceiver(address receiver) external {
+        delegateReceiver[msg.sender] = receiver;
+    }
+
+    /// @inheritdoc UUPSUpgradeable
+    function _authorizeUpgrade(address) internal view override onlyGovernorUpgrader(core) {}
+}
