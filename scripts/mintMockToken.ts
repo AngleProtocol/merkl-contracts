37eca211957e3e4d6c14b4bd3d835ce32d687784
Baptiste Guerin <baptist.guerin@gmail.com>
2024-06-03 18:57:25 +0200

    feat: add mint mock token

---
+++ scripts/mintMockToken.ts
@@ -0,0 +1,26 @@
+import { parseEther, parseUnits } from 'ethers/lib/utils';
+import { ethers } from 'hardhat';
+
+import { MAX_UINT256 } from '../test/hardhat/utils/helpers';
+import { MockToken__factory } from '../typechain';
+
+async function main() {
+  const { deployer } = await ethers.getNamedSigners();
+
+  const MockToken = MockToken__factory.connect('0xA7c167f58833c5e25848837f45A1372491A535eD', deployer);
+
+  console.log(`Minting MockToken to ${deployer.address}...`);
+  await (
+    await MockToken.mint(deployer.address, parseUnits('100000', 6), {
+      // gasLimit: 300_000,
+      // maxPriorityFeePerGas: 100e9,
+      // maxFeePerGas: 700e9,
+    })
+  ).wait();
+  console.log(`...Minted mock token to address ${deployer.address} ✅`);
+}
+
+main().catch(error => {
+  console.error(error);
+  process.exit(1);
+});
