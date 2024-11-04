bf452aeba2652abd061ccd6f3bd1155584c2e3e3
picodes <picodes@angle.money>
2024-11-04 11:33:31 +0100

    fix: deployer address

--- ignition/modules/Disputer.ts
+++ ignition/modules/Disputer.ts
@@ -2,7 +2,7 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("Disputer", (m) => {
   const distributor = '0x3Ef3D8bA38EBe18DB133cEc108f4D14CE00Dd9Ae';
-  const deployer = '0xfdA462548Ce04282f4B6D6619823a7C64Fdc0185';
+  const deployer = '0xA9DdD91249DFdd450E81E1c56Ab60E1A62651701';
   const whitelist = [
     '0xeA05F9001FbDeA6d4280879f283Ff9D0b282060e',
     '0x0dd2Ea40A3561C309C03B96108e78d06E8A1a99B',
