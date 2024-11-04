9eb6140cac1d2a0098c1f7ddc0d5a217915940cb
Nicolas Lecouflet <lecoufletnicolas@gmail.com>
2024-11-04 16:55:18 +0100

    fix: set correctly owner and deployer of disputer

--- scripts/deployDisputer.ts
+++ scripts/deployDisputer.ts
@@ -10,19 +10,20 @@ async function main() {
 
   try {
     const distributor = registry(chainId as ChainId)?.Merkl?.Distributor;
-    const deployer = registry(chainId as ChainId)?.AngleLabs || '0xA9DdD91249DFdd450E81E1c56Ab60E1A62651701';
+    const owner = registry(chainId as ChainId)?.AngleLabs || '0xA9DdD91249DFdd450E81E1c56Ab60E1A62651701';
     const whitelist = [
       '0xeA05F9001FbDeA6d4280879f283Ff9D0b282060e',
       '0x0dd2Ea40A3561C309C03B96108e78d06E8A1a99B',
       '0xF4c94b2FdC2efA4ad4b831f312E7eF74890705DA'
     ];
 
-    if(!distributor || !deployer) {
+    if(!distributor || !owner) {
       throw new Error(`No distributor or deployer address found for chain ${chainId}`);
     }
 
-    const Disputer = await ethers.getContractFactory('Disputer');
-    const disputer = await Disputer.deploy(deployer, whitelist, distributor);
+    const {deployer} = await ethers.getNamedSigners();
+    const Disputer = await ethers.getContractFactory('Disputer', deployer);
+    const disputer = await Disputer.deploy(owner, whitelist, distributor);
     
     const disputerAddress = disputer.address;
     console.log(`Successfully deployed Disputer to: ${disputerAddress}`);
