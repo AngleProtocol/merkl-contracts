df5e2038c0ce993cf6d09b0bb71e050e83087445
Nicolas Lecouflet <lecoufletnicolas@gmail.com>
2024-11-14 21:37:52 +0100

    feat: import CoreBorrow contract

--- contracts/interfaces/ICore.sol
+++ contracts/interfaces/ICore.sol
@@ -6,6 +6,12 @@ pragma solidity ^0.8.17;
 /// @author Angle Labs, Inc.
 /// @notice Interface for the `Core` contracts of smart contract modules used in Angle Labs contracts
 interface ICore {
+    /// @notice Checks if an address corresponds to a treasury of a stablecoin with a flash loan
+    /// module initialized on it
+    /// @param treasury Address to check
+    /// @return Whether the address has the `FLASHLOANER_TREASURY_ROLE` or not
+    function isFlashLoanerTreasury(address treasury) external view returns (bool);
+
     /// @notice Checks whether an address is governor
     /// @param admin Address to check
     /// @return Whether the address has the `GOVERNOR_ROLE` or not
