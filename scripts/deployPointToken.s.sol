1667f491e587bca50dcda723618c623a41009f8c
vincent-merkl <vincent@merkl.xyz>
2025-04-08 11:10:23 +0200

    feat : add deployment script (#116)
    * add deployment script
    
    * remove toggle minter from guardian capability
    
    * push token script

---
+++ scripts/deployPointToken.s.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.17;
+
+import { console } from "forge-std/console.sol";
+
+import { BaseScript } from "./utils/Base.s.sol";
+
+import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import { ITransparentUpgradeableProxy } from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { JsonReader } from "@utils/JsonReader.sol";
+import { ContractType } from "@utils/Constants.sol";
+
+import { PointToken } from "../contracts/partners/tokenWrappers/PointToken.sol";
+import { DistributionCreator } from "../contracts/DistributionCreator.sol";
+import { IAccessControlManager } from "../contracts/interfaces/IAccessControlManager.sol";
+import { MockToken } from "../contracts/mock/MockToken.sol";
+
+contract DeployPointToken is BaseScript {
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        address minter = 0x9BF9E7e728ed796F3D8F871087Ba0348C9E3adf9;
+        address accessControlManager = 0x05225a6416EDaeeC7227027E86F7A47D18A06b91;
+
+        // Deploy implementation
+        PointToken implementation = new PointToken("Apples","APPL", minter,accessControlManager);
+        console.log("PointToken Implementation:", address(implementation));
+
+        // Initialize
+        vm.stopBroadcast();
+    }
+}
