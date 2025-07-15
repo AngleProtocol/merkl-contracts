4306793bf17ff94fe59be2eb4c1da55024bacfef
Picodes <picodes@merkl.xyz>
2025-07-15 16:19:30 +0200

    fix: CI

--- scripts/deploySonicFragment.s.sol
+++ scripts/deploySonicFragment.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.17;
 
 import { console } from "forge-std/console.sol";
 
-import { BaseScript } from "./utils/Base.s.sol";.
+import { BaseScript } from "./utils/Base.s.sol";
 
 import { console } from "forge-std/console.sol";
 import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
