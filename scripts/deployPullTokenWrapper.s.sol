b64a5586fe991c8c278e40689b0f48c70533c0f5
Pablo Veyrat <veyratpablo@gmail.com>
2026-01-27 08:33:06 +0100

    fix: foundry.toml

--- scripts/deployPullTokenWrapper.s.sol
+++ scripts/deployPullTokenWrapper.s.sol
@@ -16,23 +16,23 @@ import { IAccessControlManager } from "../contracts/interfaces/IAccessControlMan
 import { MockToken } from "../contracts/mock/MockToken.sol";
 
 contract DeployPullTokenWrapper is BaseScript {
-    // forge script scripts/deployPullTokenWrapper.s.sol --rpc-url mainnet --sender 0xA9DdD91249DFdd450E81E1c56Ab60E1A62651701 --broadcast --verify
+    // forge script scripts/deployPullTokenWrapper.s.sol --rpc-url ink --sender 0xA9DdD91249DFdd450E81E1c56Ab60E1A62651701 --broadcast --verify
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
         address distributionCreator = 0x8BB4C975Ff3c250e0ceEA271728547f3802B36Fd;
         // ------------------------------------------------------------------------
         // TO EDIT
-        address underlying = 0xae78736Cd615f374D3085123A210448E74Fc6393;
-        address holder = 0x9ff471F9f98F42E5151C7855fD1b5aa906b1AF7e;
+        address underlying = 0x70A38B0c90441e991346B7A0Cd98C8528dD1c234;
+        address holder = 0xdef1FA4CEfe67365ba046a7C630D6B885298E210;
 
         // Need to choose the implementation type and if implementation needs to be deployed
 
-        // address implementation = address(new PullTokenWrapperAllow());
+        address implementation = address(new PullTokenWrapperAllow());
         // address implementation = address(new PullTokenWrapperWithdraw());
 
         // Ethereum implementation of PullTokenWrapperAllow
-        address implementation = 0x979a04fd2f3A6a2B3945A715e24b974323E93567;
+        // address implementation = 0x979a04fd2f3A6a2B3945A715e24b974323E93567;
         // Ethereum implementation of PullTokenWrapperWithdraw
         // address implementation = 0x721d37cf37e230E120a09adbBB7aAB0CF729AcA1;
 
