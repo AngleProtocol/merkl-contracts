be026a6876ff37baca38b28eb9ca6cc0f2c95b07
Pablo Veyrat <veyratpablo@gmail.com>
2026-01-27 10:05:10 +0100

    continuing deployments

--- scripts/utils/UpgradeDeploymentBase.s.sol
+++ scripts/utils/UpgradeDeploymentBase.s.sol
@@ -6,6 +6,7 @@ import { Script } from "forge-std/Script.sol";
 import { stdJson } from "forge-std/StdJson.sol";
 
 import { DistributionCreator } from "../../contracts/DistributionCreator.sol";
+import { DistributionCreatorWithDistributions } from "../../contracts/DistributionCreatorWithDistributions.sol";
 import { Distributor } from "../../contracts/Distributor.sol";
 
 /// @title UpgradeDeploymentBase
@@ -35,60 +36,66 @@ abstract contract UpgradeDeploymentBase is Script {
     /// @notice Get all supported chain configurations (SINGLE SOURCE OF TRUTH)
     /// @return Array of ChainConfig structs
     function _getChainConfigs() internal pure returns (ChainConfig[] memory) {
-        ChainConfig[] memory configs = new ChainConfig[](52);
+        ChainConfig[] memory configs = new ChainConfig[](58);
 
         configs[0] = ChainConfig("mainnet", 1, false);
-        configs[1] = ChainConfig("polygon", 137, false);
-        configs[2] = ChainConfig("fantom", 250, false);
-        configs[3] = ChainConfig("optimism", 10, false);
-        configs[4] = ChainConfig("arbitrum", 42161, false);
-        configs[5] = ChainConfig("avalanche", 43114, false);
-        configs[6] = ChainConfig("bsc", 56, false);
-        configs[7] = ChainConfig("gnosis", 100, false);
-        configs[8] = ChainConfig("polygonzkevm", 1101, false);
-        configs[9] = ChainConfig("base", 8453, false);
-        configs[10] = ChainConfig("bob", 60808, false);
-        configs[11] = ChainConfig("linea", 59144, false);
-        configs[12] = ChainConfig("mantle", 5000, false);
-        configs[13] = ChainConfig("blast", 81457, false);
-        configs[14] = ChainConfig("mode", 34443, false);
-        configs[15] = ChainConfig("thundercore", 108, false);
-        configs[16] = ChainConfig("coredao", 1116, false);
-        configs[17] = ChainConfig("xlayer", 196, false);
-        configs[18] = ChainConfig("taiko", 167000, false);
-        configs[19] = ChainConfig("fuse", 122, false);
-        configs[20] = ChainConfig("immutable", 13371, false);
-        configs[21] = ChainConfig("scroll", 534352, false);
-        configs[22] = ChainConfig("manta", 169, false);
-        configs[23] = ChainConfig("sei", 1329, false);
-        configs[24] = ChainConfig("celo", 42220, false);
-        configs[25] = ChainConfig("fraxtal", 252, false);
-        configs[26] = ChainConfig("astar", 592, false);
-        configs[27] = ChainConfig("rootstock", 30, false);
-        configs[28] = ChainConfig("moonbeam", 1284, false);
-        configs[29] = ChainConfig("skale", 2046399126, false);
-        configs[30] = ChainConfig("worldchain", 480, false);
-        configs[31] = ChainConfig("lisk", 1135, false);
-        configs[32] = ChainConfig("etherlink", 42793, false);
-        configs[33] = ChainConfig("swell", 1923, false);
-        configs[34] = ChainConfig("sonic", 146, false);
-        configs[35] = ChainConfig("corn", 21000000, false);
-        configs[36] = ChainConfig("ink", 57073, false);
-        configs[37] = ChainConfig("ronin", 2020, false);
-        configs[38] = ChainConfig("flow", 747, false);
-        configs[39] = ChainConfig("berachain", 80094, true); // Often testnet
-        configs[40] = ChainConfig("nibiru", 6900, false);
-        configs[41] = ChainConfig("zircuit", 48900, false);
-        configs[42] = ChainConfig("apechain", 33139, false);
-        configs[43] = ChainConfig("hyperevm", 999, false);
+        configs[1] = ChainConfig("optimism", 10, false);
+        configs[2] = ChainConfig("rootstock", 30, false);
+        configs[3] = ChainConfig("xdc", 50, false);
+        configs[4] = ChainConfig("bsc", 56, false);
+        configs[5] = ChainConfig("gnosis", 100, false);
+        configs[6] = ChainConfig("fuse", 122, false);
+        configs[7] = ChainConfig("unichain", 130, false);
+        configs[8] = ChainConfig("polygon", 137, false);
+        configs[9] = ChainConfig("monad", 143, false);
+        configs[10] = ChainConfig("sonic", 146, false);
+        configs[11] = ChainConfig("redbelly", 151, false);
+        configs[12] = ChainConfig("manta", 169, false);
+        configs[13] = ChainConfig("xlayer", 196, false);
+        configs[14] = ChainConfig("tac", 239, false);
+        configs[15] = ChainConfig("fraxtal", 252, false);
+        configs[16] = ChainConfig("zksync", 324, false);
+        configs[17] = ChainConfig("worldchain", 480, false);
+        configs[18] = ChainConfig("astar", 592, false);
+        configs[19] = ChainConfig("flow", 747, false);
+        configs[20] = ChainConfig("stable", 988, false);
+        configs[21] = ChainConfig("hyperevm", 999, false);
+        configs[22] = ChainConfig("lisk", 1135, false);
+        configs[23] = ChainConfig("moonbeam", 1284, false);
+        configs[24] = ChainConfig("sei", 1329, false);
+        configs[25] = ChainConfig("soneium", 1868, false);
+        configs[26] = ChainConfig("swell", 1923, false);
+        configs[27] = ChainConfig("ronin", 2020, false);
+        configs[28] = ChainConfig("citrea", 4114, false);
+        configs[29] = ChainConfig("megaeth", 4326, false);
+        configs[30] = ChainConfig("mantle", 5000, false);
+        configs[31] = ChainConfig("saga", 5464, false);
+        configs[32] = ChainConfig("nibiru", 6900, false);
+        configs[33] = ChainConfig("base", 8453, false);
+        configs[34] = ChainConfig("plasma", 9745, false);
+        configs[35] = ChainConfig("immutable", 13371, false);
+        configs[36] = ChainConfig("0g", 16661, false);
+        configs[37] = ChainConfig("corn", 21000000, false);
+        configs[38] = ChainConfig("mezo", 31612, false);
+        configs[39] = ChainConfig("apechain", 33139, false);
+        configs[40] = ChainConfig("mode", 34443, false);
+        configs[41] = ChainConfig("arbitrum", 42161, false);
+        configs[42] = ChainConfig("celo", 42220, false);
+        configs[43] = ChainConfig("etherlink", 42793, false);
         configs[44] = ChainConfig("hemi", 43111, false);
-        configs[45] = ChainConfig("xdc", 50, false);
-        configs[46] = ChainConfig("katana", 747474, true);
-        configs[47] = ChainConfig("tac", 239, false);
-        configs[48] = ChainConfig("plasma", 9745, false);
-        configs[49] = ChainConfig("mezo", 31612, false);
-        configs[50] = ChainConfig("redbelly", 151, false);
-        configs[51] = ChainConfig("saga", 5464, false);
+        configs[45] = ChainConfig("avalanche", 43114, false);
+        configs[46] = ChainConfig("zircuit", 48900, false);
+        configs[47] = ChainConfig("ink", 57073, false);
+        configs[48] = ChainConfig("linea", 59144, false);
+        configs[49] = ChainConfig("bob", 60808, false);
+        configs[50] = ChainConfig("berachain", 80094, false);
+        configs[51] = ChainConfig("blast", 81457, false);
+        configs[52] = ChainConfig("plume", 98866, false);
+        configs[53] = ChainConfig("taiko", 167000, false);
+        configs[54] = ChainConfig("scroll", 534352, false);
+        configs[55] = ChainConfig("katana", 747474, true);
+        configs[56] = ChainConfig("skale", 2046399126, false);
+        configs[57] = ChainConfig("ethereal", 5064014, false);
 
         return configs;
     }
@@ -112,10 +119,20 @@ abstract contract UpgradeDeploymentBase is Script {
     /// @return distributionCreatorImpl The deployed DistributionCreator implementation address
     /// @return distributorImpl The deployed Distributor implementation address
     function _deployImplementations() internal returns (address distributionCreatorImpl, address distributorImpl) {
-        console.log("Deploying DistributionCreator implementation...");
-        DistributionCreator dcImpl = new DistributionCreator();
-        distributionCreatorImpl = address(dcImpl);
-        console.log("DistributionCreator Implementation:", distributionCreatorImpl);
+        uint256 currentChainId = block.chainid;
+        
+        // Deploy DistributionCreatorWithDistributions for Polygon (chainId 137)
+        if (currentChainId == 137) {
+            console.log("Deploying DistributionCreatorWithDistributions implementation (Polygon)...");
+            DistributionCreatorWithDistributions dcImpl = new DistributionCreatorWithDistributions();
+            distributionCreatorImpl = address(dcImpl);
+            console.log("DistributionCreatorWithDistributions Implementation:", distributionCreatorImpl);
+        } else {
+            console.log("Deploying DistributionCreator implementation...");
+            DistributionCreator dcImpl = new DistributionCreator();
+            distributionCreatorImpl = address(dcImpl);
+            console.log("DistributionCreator Implementation:", distributionCreatorImpl);
+        }
 
         console.log("Deploying Distributor implementation...");
         Distributor dImpl = new Distributor();
