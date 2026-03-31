064808c56625a60d764c9b04dc20e64c5e3d129f
leomerkl <leo@merkl.xyz>
2026-03-31 18:34:32 +0200

    feat: add weth9 and fluent chain (#157)
    * feat: add weth9 anf fluent chain
    
    * fix: lint
    
    * fix: typos and license
    
    * fix: remove console import
    
    * fix: added licenses and typo
    
    * fix: set max warnings to 25

---
+++ scripts/deployWETH9.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.17;
+
+import {Script} from "forge-std/Script.sol";
+import {WETH9} from "../contracts/partners/tokenWrappers/weth9.sol";
+
+contract DeployWETH9 is Script {
+    WETH9 public weth9;
+    uint256 private DEPLOYER_PRIVATE_KEY;
+
+    function run() public {
+        DEPLOYER_PRIVATE_KEY = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        vm.startBroadcast(DEPLOYER_PRIVATE_KEY);
+
+        weth9 = new WETH9(); 
+
+        vm.stopBroadcast();
+    }
+}
