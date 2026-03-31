385a23eed068e99a9e91ec91804f7d4c914bae36
Léo Marché <leo@merkl.xyz>
2026-03-31 14:20:50 +0200

    fix: added licenses and typo

--- scripts/deployWETH9.s.sol
+++ scripts/deployWETH9.s.sol
@@ -8,8 +8,6 @@ contract DeployWETH9 is Script {
     WETH9 public weth9;
     uint256 private DEPLOYER_PRIVATE_KEY;
 
-    function setUp() public {}
-
     function run() public {
         DEPLOYER_PRIVATE_KEY = vm.envUint("DEPLOYER_PRIVATE_KEY");
         vm.startBroadcast(DEPLOYER_PRIVATE_KEY);
